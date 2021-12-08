32edd2d0bbcb12942c0a187342c200dda0e3dd7f
Matthew Toohey <contact@mtoohey.com>
2021-12-07 20:38:27 -0500

    refactor(day6): resolve most lsp warnings

--- day6/part1/src/day6_part1/core.clj
+++ day6/part1/src/day6_part1/core.clj
@@ -1,17 +1,25 @@
 (ns day6-part1.core
+  (:require [clojure.java.io])
+  (:require [clojure.string])
+  (:require [clojure.edn])
   (:gen-class))
 
-(defn -main
-  [& args]
-  (with-open [rdr (clojure.java.io/reader "../input")]
-    (def input (first (line-seq rdr))))
-  (def fishes (map clojure.edn/read-string (clojure.string/split input #",")))
-  (dotimes [_ 80]
-    (def new_fish '())
-    (doseq [fish fishes]
-      (if (= 0 fish)
-        (do (def new_fish (conj new_fish 6))
-            (def new_fish (conj new_fish 8)))
-        (def new_fish (conj new_fish (- fish 1)))))
-    (def fishes new_fish))
-  (println (count fishes)))
+(defn -main []
+  (println
+   (loop [fishes (map clojure.edn/read-string
+                      (clojure.string/split
+                       (with-open
+                        [rdr (clojure.java.io/reader "../input")]
+                         (first (line-seq rdr)))
+                       #","))
+          sim-days-remaining 80]
+     (if (> sim-days-remaining 0)
+       (recur
+        (flatten
+         (map (fn [fish]
+                (if (= 0 fish)
+                  '(6 8)
+                  (- fish 1)))
+              fishes))
+        (- sim-days-remaining 1))
+       (count fishes)))))
--- day6/part2/src/day6_part2/core.clj
+++ day6/part2/src/day6_part2/core.clj
@@ -1,23 +1,28 @@
 (ns day6-part2.core
+  (:require [clojure.java.io])
+  (:require [clojure.string])
+  (:require [clojure.edn])
   (:gen-class))
 
-(defn -main
-  [& args]
+(defn -main []
   (def day-map {})
-  (defn days
-    ([counter] (days counter 256))
-    ([counter days-remaining]
-     (if (>= counter days-remaining)
-       1
-       (let [new-spawned (- days-remaining counter 1)]
-         (if (contains? day-map new-spawned)
-           (get day-map new-spawned)
-           (let [new (+ (days 6 new-spawned) (days 8 new-spawned))]
-             (def day-map (assoc day-map new-spawned new))
-             new))))))
-
-  (with-open [rdr (clojure.java.io/reader "../input")]
-    (def input (first (line-seq rdr))))
   (println
-   (reduce + (map days
-                  (map clojure.edn/read-string (clojure.string/split input #","))))))
+   (reduce
+    +
+    (map
+     (fn days
+       ([counter] (days counter 256))
+       ([counter days-remaining]
+        (if (>= counter days-remaining)
+          1
+          (let [new-spawned (- days-remaining counter 1)]
+            (if (contains? day-map new-spawned)
+              (get day-map new-spawned)
+              (let [new (+ (days 6 new-spawned) (days 8 new-spawned))]
+                (def day-map (assoc day-map new-spawned new))
+                new))))))
+     (map
+      clojure.edn/read-string
+      (clojure.string/split
+       (with-open [rdr (clojure.java.io/reader "../input")]
+         (first (line-seq rdr))) #","))))))
