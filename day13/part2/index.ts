2386b19f98e8d0c3513a0d0364e17cc38c5b1996
Matthew Toohey <contact@mtoohey.com>
2021-12-14 01:23:43 -0500

    feat: day13

---
+++ day13/part2/index.ts
@@ -0,0 +1,76 @@
+import * as fs from "fs";
+
+const input = String(fs.readFileSync("../input"));
+
+const [pointsString, foldsString] = input.split("\n\n");
+
+const points = pointsString
+  .split("\n")
+  .map((line) => line.split(",").map((c) => parseInt(c)));
+const maxX = points.reduce(
+  (prev, point) => (prev > point[0] ? prev : point[0]),
+  -Infinity
+);
+const maxY = points.reduce(
+  (prev, point) => (prev > point[1] ? prev : point[1]),
+  -Infinity
+);
+
+type axis = "x" | "y";
+
+function toAxis(s: String): axis {
+  if (s === "x") {
+    return "x";
+  } else if (s === "y") {
+    return "y";
+  } else {
+    throw Error();
+  }
+}
+
+type fold = [axis, number];
+
+let folds: fold[] = foldsString
+  .split("\n")
+  .filter((line) => line !== "")
+  .map((line) => {
+    const [axisString, valueString] = line.slice(11).split("=");
+    return [toAxis(axisString), parseInt(valueString)];
+  });
+
+folds.reverse();
+
+let grid = Array(maxY + 1)
+  .fill(false)
+  .map((_) => Array(maxX + 1).fill(false));
+
+for (const point of points) {
+  grid[point[1]][point[0]] = true;
+}
+
+let fold: fold;
+while ((fold = folds.pop())) {
+  if (fold[0] === "y") {
+    const newHeight = Math.floor(grid.length / 2);
+    grid = Array(newHeight)
+      .fill(false)
+      .map((_, y) =>
+        Array(grid[0].length)
+          .fill(0)
+          .map((_, x) => grid[y][x] || grid[grid.length - y - 1][x])
+      );
+  } else {
+    const newWidth = Math.floor(grid[0].length / 2);
+    grid = Array(grid.length)
+      .fill(false)
+      .map((_, y) =>
+        Array(newWidth)
+          .fill(0)
+          .map((_, x) => grid[y][x] || grid[y][grid[0].length - x - 1])
+      );
+  }
+}
+
+console.log(
+  grid.map((row) => row.map((value) => (value ? "#" : ".")).join("")).join("\n")
+);
